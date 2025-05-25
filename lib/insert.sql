20f804019a466274dfd720897bb29ed1ac08032d
Cephas Mwai <cephasmwai561@gmail.com>
2025-05-25 14:47:13 +0300

    initial commit

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 5, 'M', 'brown', 'calm', 1),
+('Rowdy', 7, 'M', 'black', 'aggressive', 1),
+('Tabitha', 4, 'F', 'golden', 'friendly', 1),
+('Sergeant Brown', 8, 'M', 'brown', 'serious', 1),
+('Melissa', 6, 'F', 'white', 'gentle', 1),
+('Grinch', 3, 'M', 'green', 'grumpy', 1),
+('Wendy', 2, 'F', 'grey', 'shy', 1),
+(NULL, 10, 'F', 'unknown', 'mysterious', 0);
